2d2b2a6873d62d7c021dace8fe1a5a5846d9f222
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-05-19 09:06:09 +0700

    Bring support to deleted column

--- src/common/user-audit-hook.ts
+++ src/common/user-audit-hook.ts
@@ -1,30 +1,48 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
+const defaultCreatedColumn = 'createdBy';
+const defaultUpdatedColumn = 'updatedBy';
+const defaultDeletedColumn = 'deletedBy';
+const defaultUserProperty = 'email';
 
-export default (): Hook => {
+export default ({
+  createdColumn = defaultCreatedColumn,
+  updatedColumn = defaultUpdatedColumn,
+  deletedColumn = defaultDeletedColumn,
+  userProperty = defaultUserProperty
+} = {}): Hook => {
   return async (context: HookContext) => {
-    const { data, method, type, params } = context;
+    const { data, method, service, type, params } = context;
+    const { disableSoftDelete } = params;
     if (type === 'after') {
       // only valid use in before hooks
       return context;
     }
-    if (method !== 'create' && method !== 'update' && method !== 'patch') {
+    if (method !== 'create' && method !== 'update' && method !== 'patch' && method !== 'remove') {
       return context;
     }
     const result: any = {};
-    const paramUser = 'email';
+    const paramUser = userProperty;
     const user = params.user;
     let userModifier = 'SYSTEM';
     if (user && user[paramUser]) {
       userModifier = user[paramUser];
     }
     if (method === 'create') {
-      const paramColumn = 'createdBy';
+      const paramColumn = createdColumn;
       result[paramColumn] = userModifier;
     } else if (method === 'update' || method === 'patch') {
-      const paramColumn = 'updatedBy';
+      const paramColumn = updatedColumn;
       result[paramColumn] = userModifier;
+    } else if (method === 'remove' && !disableSoftDelete) {
+      const paramColumn = deletedColumn;
+      result[paramColumn] = userModifier;
+      const updatedData = Object.assign(data, result);
+      const id = context.id || null;
+      const final = await service.patch(id, updatedData, params);
+      context.result = final;
+      return context;
     }
     context.data = Object.assign(data, result);
     return context;
