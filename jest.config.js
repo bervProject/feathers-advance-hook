a2e43d3690a80b3b069e2d0928e80ad02ab6e55a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-04-04 22:01:23 +0700

    feat!: update feathers 5.0 compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      diagnostics: false
-    }
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      // required due to custom location of tsconfig.json configuration file
+      // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+      { diagnostics: false },
+    ],
   },
   collectCoverage: true,
   collectCoverageFrom: [
